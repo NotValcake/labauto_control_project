e9cb0f82a3bfe77dcb9903d609a549df089d6c2c
pietro-agnelli <p.agnelli@studenti.unibs.it>
2025-08-02 11:39:42 +0200

    compute control action and simulation

--- project_part_2/main.py
+++ project_part_2/main.py
@@ -32,19 +32,19 @@ def main():
 
     # Controller creation
     rc_controller = RCController(Tc, lp_filter, delay)
+    rc_controller.initialize()
 
     q, qd, qdd, u, t = [], [], [], [], []
     actual_time = 0.0
 
+    # Free system evolution
     for i in range(0, 2000):
-
         sys.simulate()
         q.append(sys.x[0])
         qd.append(sys.x[1])
         qdd.append(sys.state_function(sys.x, sys.u, actual_time)[1])
         u.append(sys.u)
         t.append(actual_time)
-
         actual_time += Tc
 
 
@@ -67,6 +67,58 @@ def main():
 
     # Torque (Control input)
     axes[1, 1].plot(time_array, u_array[:], label='Torque')
+
+    # Formatting
+    for ax in axes.flat:
+        if ax.has_data():
+            ax.grid()
+            ax.legend()
+            ax.set_xlabel("Time (s)")
+
+    plt.tight_layout()
+    plt.show()
+
+    # Controlled system output (1st spec)
+    q, qd, qdd, u, t , ref= [], [], [], [], [], []
+    actual_time = 0.0
+    w = 10 #rad/s
+    rc_controller.starting(0, sys.read_sensor_value(), 0)
+    for i in range(0, 2000):
+        r =w*np.sin(w * actual_time)    
+        uc = rc_controller.compute_control_action(r, sys.read_sensor_value())
+        sys.write_actuator_value([uc])
+        sys.simulate()
+        q.append(sys.x[0])
+        qd.append(sys.x[1])
+        qdd.append(sys.state_function(sys.x, [uc], actual_time)[1])
+        u.append(uc)
+        t.append(actual_time)
+        ref.append(r)
+
+        actual_time += Tc
+
+    time_array = np.array(t)
+    q_array = np.array(q)
+    qd_array = np.array(qd)
+    qdd_array = np.array(qdd)
+    u_array = np.array(u)
+    r_array = np.array(ref)
+
+    fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+
+    #Error
+    axes[0, 0].plot(time_array,(r_array[:]- qd_array[:]), label='Velocity error')
+    axes[0, 0].legend(loc='best')
+    # Velocity
+    axes[1, 0].plot(time_array, r_array[:] ,label = 'Reference signal w = 10rad/s')
+    axes[1, 0].plot(time_array, qd_array[:], label='Controlled velocity')
+    axes[1, 0].legend(loc='best')
+    # Acceleration
+    axes[0, 1].plot(time_array, qdd_array[:], label='Acceleration')
+
+    # Torque (Control input)
+    axes[1, 1].plot(time_array, u_array[:], label='Control Input')
+    axes[1,1].legend(loc = 'best')
 
     # Formatting
     for ax in axes.flat:
